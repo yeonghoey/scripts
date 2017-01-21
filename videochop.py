af9c8436e847ce15a25ea37d1b47a67c84f751da
yeonghoey <yeonghoey@gmail.com>
2017-01-21 18:39:17 +0900

    Support video only chop

--- videochop.py
+++ videochop.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, print_function, unicode_literals
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
 
 from math import ceil, floor
 import multiprocessing
@@ -31,13 +32,15 @@ def build_plan(srcpattern, dstdir, seconds):
     vpaths = detect_targets(srcdir, pattern, ['.avi', '.mp4', '.mkv'])
     spaths = detect_targets(srcdir, pattern, ['.srt'])
     plan = []
-    for key in vpaths:
-        if key not in spaths:
-            errexit('Failed to find srt for "%s"' % vpaths[key])
-        vpath = vpaths[key]
-        spath = spaths[key]
+    for key, vpath in vpaths.viewitems():
+        if key in spaths:
+            spath = spaths[key]
+            slices = compute_slices_by_srt(spath, seconds)
+        else:
+            spath = None
+            slices = compute_slices_by_duration(vpath, seconds)
+
         outbase = os.path.join(dstdir, key)
-        slices = compute_slices(spath, seconds)
         plan.append((vpath, spath, outbase, slices))
     return sorted(plan, key=lambda t: t[2])
 
@@ -55,16 +58,16 @@ def detect_targets(srcdir, pattern, exts):
     return results
 
 
-def compute_slices(path, seconds, padding=1):
-    subs = pysrt.open(path)
+def compute_slices_by_srt(spath, seconds, padding=1):
+    subs = pysrt.open(spath)
     seconds, padding = {'seconds': seconds}, {'seconds': padding}
     head, intervals = None, []
     for s in subs:
         if head is None:
-            head = s.start - padding
+            head = s.start - padding  # SubRipTime clamps negatives to zero
         term = s.end - head
         if term >= seconds:
-            tail = s.end + padding
+            tail = s.end + padding  # ffmpeg clamps ranges into durations
             intervals.append((head, tail))
             head = None
 
@@ -79,14 +82,39 @@ def compute_slices(path, seconds, padding=1):
             for a, b in intervals]
 
 
+def compute_slices_by_duration(vpath, seconds, padding=1):
+    pos, duration = 0, read_duration(vpath)
+    intervals = [(max(0, i*seconds-padding),
+                  min((i+1)*seconds+padding, duration))
+                 for i in range(duration//seconds)]
+
+    s, e = intervals[-1]
+    remainings = duration % seconds
+    intervals[-1] = (s, e + remainings)
+    return intervals
+
+
+def read_duration(vpath):
+    command = 'ffmpeg -i "{vpath}" 2>&1 | '\
+              'grep -o "Duration: \d\d:\d\d:\d\d"'.format(vpath=vpath)
+    output = subprocess.check_output(command, shell=True)
+    mo = re.search(r'(\d\d):(\d\d):(\d\d)', output)
+    if mo is None:
+        errexit('Failed to read duration for %s' % vpath)
+    h, m, s = mo.groups()
+    return int(h)*3600 + int(m)*60 + int(s) + 1  # +1 for milliseconds
+
+
 def prompt_plan(plan):
     for vpath, spath, outbase, slices in plan:
         _, video_name = os.path.split(vpath)
-        _, srt_name = os.path.split(spath)
+
+        _, srt_name = os.path.split(spath) if spath is not None else \
+                      (None, '<no-subtitle>')
         print('{} into {} slices'.format(outbase, len(slices)))
         print('- {}'.format(video_name))
         print('- {}'.format(srt_name))
-    
+
     prompt = raw_input('Are you sure? (yes/.) ')
     if prompt.lower() != 'yes':
         errexit('Abort')
@@ -94,7 +122,8 @@ def prompt_plan(plan):
 
 def chop(vpath, spath, dstroot, start, end):
     chop_video(vpath, dstroot + '.mp4', start, end)
-    chop_srt(spath, dstroot + '.srt', start, end)
+    if spath is not None:
+        chop_srt(spath, dstroot + '.srt', start, end)
 
 
 def chop_video(vpath, outpath, start, end):
@@ -123,7 +152,7 @@ def errexit(message):
 if __name__ == '__main__':
     params = sys.argv[1:]
     if len(params) != 3:
-        errexit('Usage: videochop <srcpattern> <pattern> <dstdir> <seconds>')
+        errexit('Usage: videochop <srcpattern> <dstdir> <seconds>')
 
     srcpattern, dstdir, seconds = params[:3]
     seconds = int(seconds)
